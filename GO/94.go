53cd9d02642cead5aca3ec8fc146127e9f8a7276
ray <321582424@qq.com>
2020-08-13 15:26:48 +0800

    Binary Tree Inorder Traversal

---
+++ GO/94.go
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func travel (root * TreeNode, output * []int){
+	if(root.Left!=nil){
+		travel(root.Left,output)
+	}
+	*output=append(*output,root.Val)
+	if(root.Right!=nil){
+		travel(root.Right,output)
+	}
+}
+// solution 1
+func inorderTraversal(root *TreeNode) []int {
+	var output []int;
+	if(root!=nil){
+		travel(root,&output)
+	}
+	return output
+}
+
+//solution 2
+func inorderTraversal(root *TreeNode) []int {
+    if root == nil {
+        return []int{}
+    }
+    result := inorderTraversal(root.Left)
+    result = append(result, root.Val)
+    return append(result, inorderTraversal(root.Right)...)
+}
